300e42a2148d104a0162c950981ae6ca559b7e9b
manikanta_chittudi <maninaiduchittudi@gmail.com>
2022-09-16 16:41:14 +0530

    my initial commit of project

--- src/app/home/dailog/dailog.component.ts
+++ src/app/home/dailog/dailog.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+export interface DialogData {
+  email: string;
+  name: string;
+  userid?:string;
+}
 @Component({
   selector: 'app-dailog',
   templateUrl: './dailog.component.html',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DailogComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public dialogRef: MatDialogRef<DailogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data:DialogData,
+    private fb :FormBuilder
+    ) { }
+    formData = this.fb.group({
+      name : ['',Validators.required],
+      email : ['',[Validators.required,Validators.email]]
+    });
   ngOnInit(): void {
+    
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
-
+  production: true,
+  // url:'https://employee-details-data-default-rtdb.asia-southeast1.firebasedatabase.app/Employees.json/',
+  firebase : {
+    apiKey: "AIzaSyDCAf5p2YMEHZdDV1N5g5OUaQ9enaVDR9E",
+    authDomain: "employee-crud-app-angular.firebaseapp.com",
+    databaseURL: "https://employee-crud-app-angular-default-rtdb.firebaseio.com",
+    projectId: "employee-crud-app-angular",
+    storageBucket: "employee-crud-app-angular.appspot.com",
+    messagingSenderId: "988283847306",
+    appId: "1:988283847306:web:d85f0464034730e271e373",
+    measurementId: "G-R009T6H55S"
+  }
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
